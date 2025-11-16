24356ca21647e0aa77be19c264684c3488d0a397
hebeiabc <hb12a@qq.com>
2025-11-16 20:01:11 +0800

    项目初始化1

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://bmd.sc.cn",
+	site: "https://blog.ysop.top",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
